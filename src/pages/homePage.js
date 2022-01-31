545ba70b0ff08a04cfe33f91cfd9b0515a6849d4
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-31 19:49:54 +0530

    homePage:: Completed the Homepage

---
+++ src/pages/homePage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import AppRating from "../components/AppRating";
+import BrandsLove from "../components/BrandsLove";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import CredExperience from "../components/CredExperience";
+import CredSecurity from "../components/CredSecurity";
+import CredStory from "../components/CredStory";
+import FeelSpecial from "../components/FeelSpecial";
+import HeroSection from "../components/HeroSection";
+import MobileScroll from "../components/MobileScroll";
+import ProductShowcase from "../components/ProductShowcase";
+import WindowPeak from "../components/WindowPeak";
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <HeroSection />
+            <ProductShowcase />
+            <FeelSpecial />
+            <BrandsLove />
+            <CredExperience />
+            <MobileScroll />
+            <div className="non-mobile">
+                <WindowPeak />
+            </div>
+            <CredSecurity />
+            <CredStory />
+            <AppRating />
+            <Footer />
+        </>
+    );
+}
+
+export default HomePage;
