05c0e3f1fb339c2c9e53087e382c96fc6d312960
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-02-11 21:16:24 +0530

    footer introduced

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <div className="footer flex absolute-center max-width">
+            Made with ❤️ by <a href="https://www.linkedin.com/in/shambashib">Shambashib Majumdar</a>
+        </div>
+
+
+    );
+
+};
