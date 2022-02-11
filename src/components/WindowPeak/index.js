eec0e89cca5f924439ec0046fdb5dba14f6034d8
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-02-11 19:55:25 +0530

    completed buttons. will be uploading header next.

---
+++ src/components/WindowPeak/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./windowPeak.css";
+
+const WindowPeak = () => {
+    return <div className="window-peak"></div>;
+};
+
+export default WindowPeak;
+
+
---
+++ src/components/common/Button/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./button.css";
+
+const Button = ({ buttonText, onClick, prefix, customClass }) => {
+    return (
+        <div
+            className={`button-wrapper flex absolute-center ${customClass}`}
+            onClick={onClick}
+        >
+            {prefix}
+            {buttonText}
+        </div>
+    );
+};
+
+export default Button;
